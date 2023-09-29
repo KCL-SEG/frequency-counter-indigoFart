dc1051a647f044d7f2e3edb6daeb5f592c2d21f3
Benjamin Morka <benjamin.morka@kcl.ac.uk>
2023-09-29 01:49:57 +0100

    Create frequency counter

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,19 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    itemsSet = set(items)
+    for item in itemsSet:
+        frequencies[str(item)] = items.count(item or str(item))
+        
+    
+           
+            
+        
+           
+            
+
+    
+
     return frequencies
+
+print(frequencies(["a", "b","a", "a", "b"]))
