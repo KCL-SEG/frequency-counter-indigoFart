daf0b0990d4e893551268c132308c2ba706b50eb
Benjamin Morka <benjamin.morka@kcl.ac.uk>
2023-09-29 02:38:43 +0100

    Fix range of for loop for string conversion

--- frequencies.py
+++ frequencies.py
@@ -4,8 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    for i in range(len(items) -1):
-        items[i] = str(items[i])
+
+    for i in range(len(items)):
+        items[i] = str(items[i])   
         
     itemsSet = set(items)
     for item in itemsSet:
@@ -16,4 +17,3 @@ def frequencies(items):
 
 
 
-print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
