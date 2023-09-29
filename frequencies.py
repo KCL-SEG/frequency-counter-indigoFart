d794b8d50d82b872ac25c4f408f643d55090f3eb
Benjamin Morka <benjamin.morka@kcl.ac.uk>
2023-09-29 02:23:51 +0100

    Fix incorrect counting of non-strings

--- frequencies.py
+++ frequencies.py
@@ -4,10 +4,16 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    itemsSet = set(items)
+    stringItems = []
+    for item in items:
+        stringItems.append(str(item))
+    itemsSet = set(stringItems)
     for item in itemsSet:
-        frequencies[str(item)] = items.count(item or str(item))
+        frequencies[str(item)] = stringItems.count(item)
         
 
     return frequencies
 
+
+
+print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
